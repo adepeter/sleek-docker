34b5d431ed708b37fcd77176cc7dcb2f41c79626
Oluwaseun Peter <adepeter26@gmail.com>
2020-07-31 17:01:22 +0100

    Refactored codes

---
+++ sleekpostgres/configs/init.sql
@@ -0,0 +1,7 @@
+CREATE USER sleekfoliouser;
+CREATE DATABASE sleekfoliodb;
+GRANT ALL PRIVILEGES ON DATABASE sleekfoliodb TO sleekfoliouser;
+
+CREATE USER sleekforumuser;
+CREATE DATABASE sleekforumdb;
+GRANT ALL PRIVILEGES ON DATABASE sleekforumdb TO sleekforumuser;
